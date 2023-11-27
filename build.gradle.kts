4fd51c48603c22562bf571e8633dc363b80c2eed
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2023-11-27 11:56:20 -0700

    switch to kotlin gradle

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.0"
+	id("io.spring.dependency-management") version "1.1.4"
+	id("com.netflix.dgs.codegen") version "6.0.3"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.generateJava {
+	packageName = "com.example.spotifydemo.codegen"
+	generateClient = true
+}
+
