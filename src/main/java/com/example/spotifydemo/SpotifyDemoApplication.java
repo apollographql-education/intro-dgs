50d36d807fe52555a8141f3e08be4dee8786fe35
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2023-11-27 10:31:57 -0700

    initial setup for gradle dependencies and basic files

---
+++ src/main/java/com/example/spotifydemo/SpotifyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotifydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spotifydemo/SpotifyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotifydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
