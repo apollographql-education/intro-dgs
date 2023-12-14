2f7a6e0cab8e67cf6b329ba72a522f41ddae5af0
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2023-12-14 12:30:42 -0700

    use record instead of pojo

--- src/main/java/com/example/spotifydemo/models/Snapshot.java
+++ src/main/java/com/example/spotifydemo/models/Snapshot.java
@@ -1,25 +1,5 @@
 package com.example.spotifydemo.models;
 
-import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Snapshot {
-
-    String snapshot_id;
-
-    String error;
-
-
-    @JsonSetter("snapshot_id")
-    public void setId(String snapshot_id) {
-        this.snapshot_id = snapshot_id;
-    }
-
-    public String getId() {
-        return this.snapshot_id;
-    }
-
-    public String getError() {
-        return this.error;
-    }
-
-}
+public record Snapshot(@JsonProperty("snapshot_id") String id, String error) { }
