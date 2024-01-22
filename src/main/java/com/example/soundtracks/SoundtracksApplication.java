a716f12d0f833d7d9cac40f21eeab422896e709a
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2024-01-22 10:53:33 -0700

    rename root module

--- src/main/java/com/example/soundtracks/SoundtracksApplication.java
+++ src/main/java/com/example/soundtracks/SoundtracksApplication.java
@@ -1,4 +1,4 @@
-package com.example.spotifydemo;
+package com.example.soundtracks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
